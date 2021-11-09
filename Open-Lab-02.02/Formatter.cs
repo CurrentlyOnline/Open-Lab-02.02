eb081c8e53d34494813b90aea8e9d16072ab7d87
CurrentlyOnline <sambor433@gmail.com>
2021-11-09 21:24:10 +0100

    Function that formats double array.

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -6,7 +6,14 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+            var output = "";
+            foreach (var number in numbers)
+            {
+                var rounded = Math.Round(number, 2).ToString().Replace(',', '.');
+                output += rounded + " ";
+            }
+            var vysledok = output.Remove(output.LastIndexOf(" "));
+            Console.Write(vysledok);
         }
     }
 }
